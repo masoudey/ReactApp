b614f204450f0e43ad6bbaf6cc78c3a811b10ad5
masoud <masoud.ey@gmail.com>
2018-05-19 16:16:11 +0430

    Portfolio

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    constructor(props){
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>New App</h2>
+            </div>
+        )
+    }
+}
---
+++ src/insex.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import ReactDom from "react-dom";
+import App from "./app";
+
+const root = document.getElementById('root');
+
+ReactDom.render(<App />,root);
